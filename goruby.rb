90be50e3eef9ec27ae94567951356f5c640e4345
Lynn Deng <beednln@163.com>
2017-06-17 15:43:03 +0800

    ruby joined

---
+++ goruby.rb
@@ -0,0 +1,19 @@
+require 'ffi'
+require 'webrick'
+
+module GoModule
+  extend FFI::Library
+  ffi_lib ['mock_opr_ngx', '/home/ldeng/1/work/go/src/github.com/ldeng7/gonginx/libgonginx.so']
+  attach_function :setStr, [:string], :void
+  attach_function :startServer, [:int], :void
+end
+
+GoModule.startServer 8081
+
+server = WEBrick::HTTPServer.new Port: 8080
+server.mount_proc '/' do |req, resp|
+  GoModule.setStr req.body
+  resp.body = 'pid: ' + Process.pid.to_s + "\n"
+end
+server.start
+
